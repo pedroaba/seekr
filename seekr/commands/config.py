226479f9d9729bf051ca33cfc97394b1243c7011
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2026-07-05 17:44:10 -0300

    feat: add config ignores and show commands

--- seekr/commands/config.py
+++ seekr/commands/config.py
@@ -1,8 +1,14 @@
+import json
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
+from rich import print_json
+
 from seekr.commands.abstract import AbstractCommand
 from seekr.config import SeekrConfig
+from seekr.decorators.finish_command import finish_command_execution
+from seekr.exceptions.file import FileOrFolderDoesNotExist
+from seekr.models.path import PathModel
 
 
 class SeekrConfigCommand(AbstractCommand):
@@ -10,16 +16,53 @@ class SeekrConfigCommand(AbstractCommand):
 
     def __init__(self, parent_parser: ArgumentParser):
         self._parent_parser = parent_parser
+
+        # subcommands parsers
         self._config_parser = None
         self._set_parser = None
+        self._show_parser = None
 
         self._config = SeekrConfig.get_instance()
 
+    @finish_command_execution
     def handle(self, namespace: Namespace):
         super().handle(namespace)
 
         if namespace.config_command == "set" and namespace.config_key:
-            pass
+            paths = namespace.paths
+            nicknames = namespace.nicknames
+
+            ignores_patterns: list[PathModel] = []
+
+            for path in paths:
+                path_to_add = Path(path)
+                if not path_to_add.exists():
+                    raise FileOrFolderDoesNotExist(path_to_add)
+
+                path_str = str(path_to_add.absolute())
+                model = PathModel(path_str, is_system_path=True)
+                ignores_patterns.append(model)
+
+            for nickname in nicknames:
+                model = PathModel(nickname, is_nickname=True)
+                ignores_patterns.append(model)
+
+            if namespace.override:
+                self._config.set_property("ignores", ignores_patterns)
+            else:
+                ignores_patterns_saved: list[PathModel] = self._config.get_property(
+                    "ignores", defaults=[]).map(
+                    lambda _, value, __: PathModel.from_json(value)
+                ).get()
+
+                for path_to_add in ignores_patterns:
+                    if path_to_add not in ignores_patterns_saved:
+                        ignores_patterns_saved.append(path_to_add)
+
+                self._config.set_property("ignores", ignores_patterns_saved)
+        elif namespace.config_command == "show":
+            config_on_memory = self._config.get()
+            print_json(json.dumps(config_on_memory, indent=4))
 
     def build(self):
         subparsers = self._parent_parser.add_subparsers(dest="command",
@@ -30,6 +73,9 @@ class SeekrConfigCommand(AbstractCommand):
         config_subparsers = config_parser.add_subparsers(
             dest="config_command",
         )
+
+        # seekr config show
+        show_parser = config_subparsers.add_parser("show", help="Show configuration")
 
         # seekr config set
         set_parser = config_subparsers.add_parser("set", help="Set config values")
@@ -58,7 +104,7 @@ class SeekrConfigCommand(AbstractCommand):
             "-pn",
             "--path-nickname",
             action="extend",
-            dest="path_nicknames",
+            dest="nicknames",
             nargs="+",
             default=[],
             help=(
@@ -75,8 +121,17 @@ class SeekrConfigCommand(AbstractCommand):
             help="Replace current ignored paths instead of appending.",
         )
 
+        ignores_parser.add_argument(
+            "--no-commit",
+            action="store_true",
+            help="Commit configuration to save on your machine.",
+            default=False,
+            dest="no_commit",
+        )
+
         self._config_parser = config_parser
         self._set_parser = set_parser
+        self._show_parser = show_parser
 
         self._set_help_text_when_is_empty(config_parser, "command", "config_command")
         self._set_help_text_when_is_empty(set_parser, "config_command", "config_key")
