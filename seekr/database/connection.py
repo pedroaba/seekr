12fda3b7276ac99a402ab3fc62ab991133b4877e
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2026-07-12 22:19:44 -0300

    feat: create connection class manager

---
+++ seekr/database/connection.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import Session
+
+from seekr.database.config import get_database_url
+
+
+class SqlAlchemyConnection:
+    def __init__(self, database_uri: str | None = None):
+        self.__database_uri = database_uri or get_database_url()
+        self.__engine: Engine | None = None
+
+        self.__sessions: list[Session] = []
+
+    def connect(self) -> None:
+        if self.__engine is None:
+            self.__engine = create_engine(self.__database_uri)
+
+    def build_session(self) -> Session:
+        session = Session(bind=self.__engine)
+        self.__sessions.append(session)
+
+        return session
+
+    def close_all_sessions(self) -> None:
+        for session in self.__sessions:
+            session.close()
